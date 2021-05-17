e8d478cfef6e2167b9b3dbed9a3ffed5fbbf4f4e
mamathaguntu <mamathaguntu@gmail.com>
2021-05-16 20:52:30 -0700

    initial changes

---
+++ app/src/main/java/cmpe277/sjsu/edu/newsapp/Fragments/NewsArticles.kt
@@ -0,0 +1,7 @@
+package cmpe277.sjsu.edu.newsapp.Fragments
+
+import androidx.fragment.app.Fragment
+import cmpe277.sjsu.edu.newsapp.R
+
+class NewsArticles: Fragment(R.layout.news_articles) {
+}
---
+++ app/src/main/java/cmpe277/sjsu/edu/newsapp/UI/MainActivity.kt
@@ -0,0 +1,12 @@
+package cmpe277.sjsu.edu.newsapp.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cmpe277.sjsu.edu.newsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
