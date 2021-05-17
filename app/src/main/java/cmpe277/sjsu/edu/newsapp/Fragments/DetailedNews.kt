2c908fb0b451c1f7ac1354506ccbf247bdaed3da
mamathaguntu <mamathaguntu@gmail.com>
2021-05-16 21:44:28 -0700

    Navigation graph changes

---
+++ app/src/main/java/cmpe277/sjsu/edu/newsapp/Fragments/DetailedNews.kt
@@ -0,0 +1,7 @@
+package cmpe277.sjsu.edu.newsapp.Fragments
+
+import androidx.fragment.app.Fragment
+import cmpe277.sjsu.edu.newsapp.R
+
+class DetailedNews: Fragment(R.layout.detailed_news) {
+}
--- app/src/main/java/cmpe277/sjsu/edu/newsapp/UI/MainActivity.kt
+++ app/src/main/java/cmpe277/sjsu/edu/newsapp/UI/MainActivity.kt
@@ -2,11 +2,18 @@ package cmpe277.sjsu.edu.newsapp.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import cmpe277.sjsu.edu.newsapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val newsNavHostFragment: NavHostFragment = findViewById(R.id.newsNavHostFragment)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
